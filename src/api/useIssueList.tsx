50d3339c4e6c1249300d6da6717f071c8e92491e
heowan <101536766+heowan@users.noreply.github.com>
2023-08-30 14:21:50 +0900

    feat: 이슈목록 ui 구현

--- src/api/useIssueList.tsx
+++ src/api/useIssueList.tsx
@@ -4,12 +4,16 @@ import axios from 'axios';
 const useIssueList = (pageNumber: number) => {
 	const [issue, setIssue] = useState([]);
 	const [error, setError] = useState(false);
+	const token = process.env.GITHUB_TOKEN;
 
 	useEffect(() => {
 		const issueListApi = async () => {
 			try {
 				const response = await axios.get(
 					`https://api.github.com/repos/facebook/react/issues?per_page=30&page=${pageNumber}&sort=comments`,
+					{
+						headers: { Authorization: token },
+					},
 				);
 				setIssue((prevIssue: any): any => {
 					return [...prevIssue, ...response.data];
--- src/pages/IssueList.tsx
+++ src/pages/IssueList.tsx
@@ -1,5 +1,31 @@
+import React, { useState } from 'react';
+import useIssueList from '../api/useIssueList';
+import IssueItem from '../components/issue/IssueItem';
+
 function IssueList() {
-	return <p>이슈목록 페이지 입니다.</p>;
+	const [pageNumber, setPageNumber] = useState(1);
+	const { issue, error } = useIssueList(pageNumber);
+
+	interface Issue {
+		id: number;
+		number: number;
+		title: string;
+		user: {
+			login: string;
+		};
+		updated_at: string;
+		comments: number;
+	}
+
+	return (
+		<div>
+			<ul>
+				{issue.map((item: Issue) => (
+					<IssueItem key={item.id} item={item} />
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default IssueList;
